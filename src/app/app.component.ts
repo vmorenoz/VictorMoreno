bb81ab147ad4fd12aef550c04b92e554ba0a0743
Victor Moreno <victor.moreno.zapata@bbva.com>
2024-04-19 20:17:22 -0500

    feat: creation of presentation layer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {FinancialProductService} from "@domain/services/financial-product.service";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'VictorMoreno';
-
-  constructor(private readonly financialProductService: FinancialProductService) {
-  }
-
-  ngOnInit() {
-    this.financialProductService.listProducts()
-      .subscribe((data) => {
-        console.log(data);
-      });
-  }
+export class AppComponent {
 }
